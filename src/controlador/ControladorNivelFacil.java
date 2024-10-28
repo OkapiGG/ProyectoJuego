75c0ebd4469565dc34e5c5134618f24db596f9c0
Emanuel-magic <emanuel.perez87@unach.mx>
2024-10-28 06:54:45 -0600

    Controladores Y Vistas

--- src/controlador/ControladorNivelFacil.java
+++ src/controlador/ControladorNivelFacil.java
@@ -3,24 +3,35 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import vista.Instrucciones;
+import vista.NivelDificil;
 import vista.NivelFacil;
 
 public class ControladorNivelFacil implements ActionListener {
 
-    NivelFacil objControladorNivelFacil;
+    NivelFacil objNivelFacil;
+    NivelDificil objNivelDificil;
 
     public ControladorNivelFacil(NivelFacil objControladorNivelFacil) {
-        this.objControladorNivelFacil = objControladorNivelFacil;
-        this.objControladorNivelFacil.jButton10.addActionListener(this);
+        this.objNivelFacil = objControladorNivelFacil;
+        this.objNivelFacil.jButton10.addActionListener(this);        
+        objNivelDificil = new NivelDificil();
+        this.objNivelFacil.jButton11.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == this.objControladorNivelFacil.jButton10) {
+        if (e.getSource() == this.objNivelFacil.jButton10) {
             Instrucciones objInstrucciones = new Instrucciones();
             objInstrucciones.setVisible(true);
+
+            this.objNivelFacil.dispose();
+        }
+
+        if (e.getSource() == this.objNivelFacil.jButton11) {
+            NivelDificil objNivelDificil = new NivelDificil();
+            objNivelDificil.setVisible(true);
             
-            this.objControladorNivelFacil.dispose();
+            this.objNivelDificil.dispose();
         }
     }
 
